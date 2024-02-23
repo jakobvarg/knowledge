19f62e9d7bb76d9341f6d314350a511bf8d78c8f
jakobvarg <155798912+jakobvarg@users.noreply.github.com>
2024-02-23 16:38:15 +0530

    If else and for loop

---
+++ ifElseTest.py
@@ -0,0 +1,15 @@
+#this is the code to understand the if ..
+
+if 10 > 5:
+    print("10 is greater than 5")
+else:
+    print("5 is greater than 10")
+    
+    
+thisList = ["Apple", "Orange", "Cherries"]
+thisList.append("Watermelon")
+
+print(thisList)
+
+for itm in thisList:
+    print(itm)
